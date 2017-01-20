66a29d0737a1b0b31ed6320a060a8c5bc0bc5edd
jchoi <jchoi@172.20.10.12>
2017-01-20 12:10:23 +0900

    사용자 이미지 프로파일 업로드 로케이션 정보 추가 - gemmy.upload.profile.locatoin

--- src/main/java/com/gemmystar/api/user/GstarUserController.java
+++ src/main/java/com/gemmystar/api/user/GstarUserController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -51,6 +52,9 @@ public class GstarUserController {
 	
 	@Autowired
 	private UserDao userDao;
+	
+	@Value("${gemmy.upload.profile.location}")
+	private String profileUploadPath;
 
 	/**
 	 * <pre>
